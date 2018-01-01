428225ea74660a501c8f254fd0cbe438f2787bbc
Eric Chung <rchung.eric@gmail.com>
2018-01-01 16:17:11 +1100

    committing untracked files

---
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Chicken Salad',
+      'This is a delicious salad.',
+      'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'
+    ),
+    new Recipe(
+      'Cereal',
+      'Crunchy.', 
+      'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
